8612d92148c27b30386876e4dff42b5ceb351978
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-06 17:11:28 +0300

    Ku-011 | Added first level of header

--- frontend/src/app/public/public-pages/main-page/main-page.component.ts
+++ frontend/src/app/public/public-pages/main-page/main-page.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@shared/components/ui/header/header.component';
 
 @Component({
-  selector: 'ku-main-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'ku-main-page',
+	standalone: true,
+	imports: [CommonModule, HeaderComponent],
+	templateUrl: './main-page.component.html',
+	styleUrls: ['./main-page.component.sass'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainPageComponent {
 
