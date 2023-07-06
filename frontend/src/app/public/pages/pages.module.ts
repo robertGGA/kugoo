18c3ddba942d18ca1f8c8bbd4ca9679ac92d3c2b
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-06 15:46:53 +0300

    Ku-010 | Added main page to pages.module.ts

--- frontend/src/app/public/pages/pages.module.ts
+++ frontend/src/app/public/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagesRoutingModule } from '@app/public/pages/pages-routing.module';
 
 
 @NgModule({
 	declarations: [],
 	imports: [
-		CommonModule
+		CommonModule,
+		PagesRoutingModule
 	]
 })
 export class PagesModule {
