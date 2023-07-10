35248c9c0139f078022239ae026a70653baa5ef7
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-10 23:33:51 +0300

    Ku-012 | Added completed header (#13)
    * Ku-012 | Fixed styles of header-wrapper
    
    * Ku-012 | Added button component and mixins
    
    * Ku-012 | Added button component

---
+++ frontend/src/app/shared/components/ui/button/button.component.ts
@@ -0,0 +1,46 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	ElementRef,
+	EventEmitter,
+	inject,
+	Input,
+	Output,
+	Renderer2,
+	ViewChild,
+	ViewEncapsulation
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonThemes } from '@shared/components/ui/button/models/button.types';
+
+@Component({
+	selector: 'ku-button',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.sass'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None
+})
+export class ButtonComponent {
+
+	@Input() theme: ButtonThemes = 'primary';
+	@Output() click = new EventEmitter();
+	@ViewChild('buttonRef') buttonRef!: ElementRef;
+	renderer = inject(Renderer2);
+
+	constructor(private elementRef: ElementRef) {
+	}
+
+	submitEvent($event: any) {
+		this.click.emit($event);
+	}
+
+	getThemeClass(): ButtonThemes {
+		return this.theme;
+	}
+
+	setCurrentTheme(theme: ButtonThemes) {
+		this.theme = theme;
+	}
+}
