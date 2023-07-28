763c816f25bceed6434c0ef22df7720d17ce5ee2
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-28 21:31:13 +0300

    Ku-023 | Added email regex & fixed buttons

--- frontend/src/app/shared/components/ui/footer/footer.component.ts
+++ frontend/src/app/shared/components/ui/footer/footer.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '@shared/components/ui/button/button.component';
+import { emailRegex } from '@app/utils/regex.helper';
 
 @Component({
 	selector: 'ku-footer',
@@ -9,13 +10,17 @@ import { ButtonComponent } from '@shared/components/ui/button/button.component';
 	imports: [CommonModule, ButtonComponent, ReactiveFormsModule],
 	templateUrl: './footer.component.html',
 	styleUrls: ['./footer.component.sass'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
-	emailControl = new FormControl('');
+	emailControl: FormControl<string | null>;
+
+	constructor() {
+		this.emailControl = new FormControl<string | null>(null, [Validators.pattern(emailRegex), Validators.required]);
+	}
 
 	onSubmitEmail(): void {
-		console.log(this.emailControl.value);
+		console.log(this.emailControl.errors);
 	}
 
 }
