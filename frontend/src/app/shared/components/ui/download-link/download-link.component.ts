25f6c856d3f4deb9e3dcc862339a55c05d0644ef
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-30 13:35:41 +0300

    Ku-025 | Added pre-bottom footer

---
+++ frontend/src/app/shared/components/ui/download-link/download-link.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+	selector: 'ku-download-link',
+	standalone: true,
+	imports: [CommonModule, RouterLink],
+	templateUrl: './download-link.component.html',
+	styleUrls: ['./download-link.component.sass'],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DownloadLinkComponent {
+	@Input() isSmall: boolean = false;
+	@Input() link: string | null = null;
+}
