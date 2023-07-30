4ceb2d4ecd1e63e86452b2486f4f63c1a0bb67be
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-30 13:37:30 +0300

    Ku-025 | Added pre-bottom footer  (#26)
    * Ku-023 | Added email regex & fixed buttons
    
    * Ku-024 | Added middle of footer
    
    * Ku-025 | Added pre-bottom footer

--- frontend/src/app/shared/components/ui/footer/footer.component.ts
+++ frontend/src/app/shared/components/ui/footer/footer.component.ts
@@ -3,17 +3,44 @@ import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '@shared/components/ui/button/button.component';
 import { emailRegex } from '@app/utils/regex.helper';
+import { IconComponent } from '@shared/components/ui/icon/icon.component';
+import { DownloadLinkComponent } from '@shared/components/ui/download-link/download-link.component';
+
+export interface LinksButtonInterface {
+	link: string,
+	iconName: string,
+	title: string,
+	count: number
+}
 
 @Component({
 	selector: 'ku-footer',
 	standalone: true,
-	imports: [CommonModule, ButtonComponent, ReactiveFormsModule],
+	imports: [CommonModule, ButtonComponent, ReactiveFormsModule, IconComponent, DownloadLinkComponent],
 	templateUrl: './footer.component.html',
 	styleUrls: ['./footer.component.sass'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
 	emailControl: FormControl<string | null>;
+
+	links: LinksButtonInterface[] = [{ link: '/', iconName: 'vk', title: 'Вконтакте', count: 3120 }, {
+		link: '/',
+		iconName: 'inst',
+		title: 'Instagram',
+		count: 10320
+	}, {
+		link: '/',
+		iconName: 'telegram',
+		title: 'Telegram',
+		count: 3200
+	},
+		{
+			link: '/',
+			iconName: 'youtube',
+			title: 'Youtube',
+			count: 3921
+		}]
 
 	constructor() {
 		this.emailControl = new FormControl<string | null>(null, [Validators.pattern(emailRegex), Validators.required]);
