937da526441fb62a5da9da26a955e20860566f78
robert <78978033+robertGGA@users.noreply.github.com>
2023-08-20 23:25:13 +0300

    Ku-039 | Added first block of scooter page with image (#41)

--- frontend/src/app/public/pages/scooter-page/scooter-page.component.ts
+++ frontend/src/app/public/pages/scooter-page/scooter-page.component.ts
@@ -1,11 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'ku-scooter-page',
-  templateUrl: './scooter-page.component.html',
-  styleUrls: ['./scooter-page.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'ku-scooter-page',
+	templateUrl: './scooter-page.component.html',
+	styleUrls: ['./scooter-page.component.sass'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ScooterPageComponent {
+	images: Array<string> = [];
+	chooseImage: string = 'assets/preview-images/samokat.png';
 
+	constructor() {
+		this.images = [
+			'assets/preview-images/small/img.png',
+			'assets/preview-images/small/img_1.png',
+			'assets/preview-images/small/img_2.png',
+			'assets/preview-images/small/img_3.png',
+			'assets/preview-images/small/img_4.png',
+			'assets/preview-images/small/img_5.png',
+			'assets/preview-images/small/img_6.png'
+		]
+	}
+
+	changeImage(url: string): void {
+		this.chooseImage = url;
+	}
+
+	identify(item: string): string {
+		return item;
+	}
 }
