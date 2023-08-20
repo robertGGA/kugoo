e5015c53a81b9ebe77ecf41d11e662f655caa856
robert <78978033+robertGGA@users.noreply.github.com>
2023-08-20 16:01:48 +0300

    Ku-037 | Added bread crumbs

---
+++ frontend/src/app/public/pages/scooter-page/scooter-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ku-scooter-page',
+  templateUrl: './scooter-page.component.html',
+  styleUrls: ['./scooter-page.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ScooterPageComponent {
+
+}
