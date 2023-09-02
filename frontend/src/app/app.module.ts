45f4c05aa3bbc7128f71948a3fefdd9dcfb2b4c3
robert <78978033+robertGGA@users.noreply.github.com>
2023-09-03 01:48:02 +0300

    Ku-045 | Added login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { IconComponent } from '@shared/components/ui/icon/icon.component';
+import { ButtonComponent } from '@shared/components/ui/button/button.component';
 
 @NgModule({
 	declarations: [
@@ -16,7 +18,9 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 		BrowserModule,
 		AppRoutingModule,
 		CoreModule,
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		IconComponent,
+		ButtonComponent
 	],
 	providers: [],
 	bootstrap: [AppComponent]
