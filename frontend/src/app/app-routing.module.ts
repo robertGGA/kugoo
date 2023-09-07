754aa44e80e74928cd95a2c7b4e0cc816794ee7f
robert <78978033+robertGGA@users.noreply.github.com>
2023-09-08 02:27:01 +0300

    Ku-047 | Minor Fixes (#49)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '@shared/components/layouts/layout/layout.component';
-import { LoginPageComponent } from '@app/pages/login-page/login-page.component';
 
 const routes: Routes = [
-	{
-		path: 'login',
-		component: LoginPageComponent
-	},
 	{
 		path: 'admin',
 		loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
--- frontend/src/app/shared/components/ui/preview-card/preview-card.component.ts
+++ frontend/src/app/shared/components/ui/preview-card/preview-card.component.ts
@@ -4,11 +4,13 @@ import { IconComponent } from '@shared/components/ui/icon/icon.component';
 import { ButtonComponent } from '@shared/components/ui/button/button.component';
 import { PreviewCardTypes } from '@shared/components/ui/preview-card/preview-card.types';
 import { RouterLink } from '@angular/router';
+import { ChipComponent } from '@shared/components/ui/chip/chip.component';
+import { ChipOptions } from '@shared/components/ui/chip/chip.types';
 
 @Component({
 	selector: 'ku-preview-card',
 	standalone: true,
-	imports: [CommonModule, IconComponent, ButtonComponent, RouterLink],
+	imports: [CommonModule, IconComponent, ButtonComponent, RouterLink, ChipComponent],
 	templateUrl: './preview-card.component.html',
 	styleUrls: ['./preview-card.component.sass'],
 	changeDetection: ChangeDetectionStrategy.OnPush
@@ -19,6 +21,10 @@ export class PreviewCardComponent {
 
 	get isNew(): boolean | undefined {
 		return this.cardData.isNew;
+	}
+
+	get chipOptions(): ChipOptions | null {
+		return this.isNew ? null : {isUpperCase: true}
 	}
 
 }
