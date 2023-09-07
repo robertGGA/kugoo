fe4d487928b92b8cc2b0621f2f7fa153e92f0b6c
robert <78978033+robertGGA@users.noreply.github.com>
2023-09-08 02:00:31 +0300

    Ku-046 | Added admin layout (#48)

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from '@app/pages/login-page/login-page.component';
+import { AdminLayoutComponent } from '@shared/components/layouts/admin-layout/admin-layout.component';
+import { AdminMainPageComponent } from '@app/admin/pages/admin-main-page/admin-main-page.component';
 
 const routes: Routes = [
-
+	{
+		path: 'login',
+		component: LoginPageComponent
+	},
+	{
+		path: '',
+		component: AdminLayoutComponent,
+		children: [
+			{
+				path: '',
+				component: AdminMainPageComponent
+			}
+		]
+	}
 ];
 
 @NgModule({
