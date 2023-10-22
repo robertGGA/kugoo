123f1c88e9f7dda977ae5bec9312850841a0f2cc
robert <78978033+robertGGA@users.noreply.github.com>
2023-10-22 20:20:13 +0300

    Ku-053 | Added new input component

--- frontend/src/app/utils/regex.helper.ts
+++ frontend/src/app/utils/regex.helper.ts
@@ -1,1 +1,1 @@
-export const emailRegex: RegExp = /^[a-zA-Z0-9а-яА-Я.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9а-яА-Я-]+\.[a-zA-Zа-яА-Я]{2,}$/;
+export const emailRegex = /^[a-zA-Z0-9а-яА-Я.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9а-яА-Я-]+\.[a-zA-Zа-яА-Я]{2,}$/;
